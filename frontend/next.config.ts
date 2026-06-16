9303a0498f75905600d49fba4b811b2ec58b210e
jitendar-dhariwal-07 <jitendardhariwalv@gmail.com>
2026-06-16 22:08:29 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/CIVIO",
+ 
   reactStrictMode: true,
   images: {
     domains: ["localhost", "api.citizenai.in", "tile.openstreetmap.org"],
-    unoptimized: true,
+    unoptimized: false,
   },
   transpilePackages: ["lucide-react"],
 };
